871de7c7e679908ed6ff270f395ade025ebf8024
Dave Fang <dave.fang@outlook.com>
2018-08-16 14:34:14 +0800

    [add] upload the header file of `trace.cpp`

---
+++ protocol_icmp/trace.h
@@ -0,0 +1,31 @@
+//
+// Created by dave on 18-8-15.
+//
+
+#ifndef CPP_SOCKET_TRACE_H
+#define CPP_SOCKET_TRACE_H
+
+#include <netdb.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <iostream>
+#include <iomanip>
+
+#include <arpa/inet.h>
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/socket.h>
+#include <sys/file.h>
+#include <sys/time.h>
+
+#include <netinet/in_systm.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+
+#endif //CPP_SOCKET_TRACE_H
